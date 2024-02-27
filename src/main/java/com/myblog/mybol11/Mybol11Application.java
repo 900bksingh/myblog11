5b2e165bfa38024779e3989a004df027a0f9ed58
900bksingh <900bksingh@gmail.com>
2024-02-26 20:42:46 -0800

    127:Create save post Feature

---
+++ src/main/java/com/myblog/mybol11/Mybol11Application.java
@@ -0,0 +1,13 @@
+package com.myblog.mybol11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybol11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybol11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myblog/mybol11/Mybol11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.mybol11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybol11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
