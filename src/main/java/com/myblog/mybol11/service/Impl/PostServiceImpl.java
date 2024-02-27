7d4f349c6d19b1d35fb08dd7f8af7bef13a2de3e
900bksingh <900bksingh@gmail.com>
2024-02-27 15:14:18 -0800

    127:Create save post Feature Entity class added 4th jan 2024

---
+++ src/main/java/com/myblog/mybol11/service/Impl/PostServiceImpl.java
@@ -0,0 +1,31 @@
+package com.myblog.mybol11.service.Impl;
+
+import com.myblog.mybol11.entity.Post;
+import com.myblog.mybol11.payload.PostDto;
+import com.myblog.mybol11.repository.PostRepository;
+import com.myblog.mybol11.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savedPost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+        dto.setTitle(savedPost.getTitle());
+        dto.setContent(savedPost.getContent());
+        return dto;
+    }
+}
---
+++ src/main/java/com/myblog/mybol11/service/PostService.java
@@ -0,0 +1,8 @@
+package com.myblog.mybol11.service;
+
+import com.myblog.mybol11.payload.PostDto;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto);
+}
+
